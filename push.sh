0723eccb898564a0bd0475e379d87d2a8efd227d
Gofer <gofer.ex.studio@gmail.com>
2024-02-23 23:18:00 +0900

    イメージのタグ生成方法を修正

--- push.sh
+++ push.sh
@@ -4,13 +4,15 @@ set -xe
 
 USER='goferex'
 
+REPOSITORY_NAME='php-fpm'
 IMAGE_BASE='debian'
 GIT_TAG='20240223001'
+
 IMAGE_TAG="${IMAGE_BASE}-${GIT_TAG}"
 SHORTHAND='debian'
 
 docker login
-docker tag "php-fpm:${IMAGE_TAG}" "${USER}/php-fpm:${IMAGE_TAG}"
-docker push "${USER}/php-fpm:${IMAGE_TAG}"
-docker tag "php-fpm:${IMAGE_TAG}" "${USER}/php-fpm:${SHORTHAND}"
-docker push "${USER}/php-fpm:${SHORTHAND}"
+docker tag "${REPOSITORY_NAME}:${IMAGE_TAG}" "${USER}/${REPOSITORY_NAME}:${IMAGE_TAG}"
+docker push "${USER}/${REPOSITORY_NAME}:${IMAGE_TAG}"
+docker tag "${REPOSITORY_NAME}:${IMAGE_TAG}" "${USER}/${REPOSITORY_NAME}:${SHORTHAND}"
+docker push "${USER}/${REPOSITORY_NAME}:${SHORTHAND}"
