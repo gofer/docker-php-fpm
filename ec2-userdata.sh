01657fd893de9ad35a890eb00434e696a2dffdf1
Gofer <gofer.ex.studio@gmail.com>
2024-02-25 16:55:21 +0900

    ビルドスクリプト自体に依存関係追加コマンドがあったのを削除

--- ec2-userdata.sh
+++ ec2-userdata.sh
@@ -9,3 +9,15 @@ yum install -y git docker tmux
 systemctl enable docker && systemctl start docker
 
 usermod -aG docker ec2-user
+
+su ec2-user
+
+cd /home/ec2-user
+
+git clone https://github.com/gofer/docker-php-fpm
+
+cd /home/ec2-user/docker-php-fpm
+
+if [ -f requirements.txt ]; then
+  pip3 install -t vendor -r requirements.txt
+fi
